940723b84523fe3dde467f31bf6070a17e145069
小林舜 <kobayashishun@MacBook-Air-kobayashishun.local>
2022-01-19 23:59:07 +0900

    OpenGLWindowの実装

---
+++ 020_Dragon/Example.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+例題プログラム：この例題を改変して大きなプログラムを作る足がかりにしてください。
+"""
+
+__author__ = 'AOKI Atsushi'
+__version__ = '0.1.0'
+__date__ = '2019/06/27 (Created: 2016/11/11)'
+
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QWidget
+
+from jp.ac.kyoto_su.cse.ap.python.Trace import Trace    # トレース情報出力のON/OFFに用います。
+from jp.ac.kyoto_su.cse.ap.python.Trace import trace    # トレース情報出力のための関数です。
+
+def main():
+	"""
+	PyQt5のQOpenGLWidgetを利用した三次元グラフィックス（OpenGL）のモデル・ビュー・コントローラ（MVC：Model-View-Controller）の例題（メイン・プログラム）です。
+	常に0を応答します。それが結果（リターンコード：終了ステータス）になることを想定しています。
+	"""
+	# Trace.trace_off()
+	Trace.trace_on()
+	trace(main)
+
+	# アプリケーションのインスタンスを生成します。
+	application = QApplication(sys.argv)
+
+	# ウィンドウ（ウェジェット）のインスタンスを生成し、ウィンドウを開きます。
+	window = QWidget()
+	window.show()
+
+	# アプリケーションのイベントループに入ります。
+	application.exec_()
+
+	# イベントループを抜けて、正常終了を意味する0を応答します。
+	return 0
+
+if __name__ == '__main__':
+	# このモジュールのmain()を呼び出し、結果を得て、Pythonシステムに終わりを告げます。
+	sys.exit(main())
