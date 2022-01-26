80dbed34f976678c8c5ca0c2a12778aa549f85a0
Kobayashi <shun123k@gmail.com>
2022-01-26 17:13:55 +0900

    042_Dragonと同じ

---
+++ 043_Dragon/jp/ac/kyoto_su/cse/ap/python/OpenGLMVC/MVC/OpenGLController.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+例題プログラム：この例題を改変して大きなプログラムを作る足がかりにしてください。
+"""
+
+__author__ = 'AOKI Atsushi'
+__version__ = '0.4.2'
+__date__ = '2019/06/30 (Created: 2016/11/11)'
+
+import sys
+
+from jp.ac.kyoto_su.cse.ap.python.Trace import trace
+
+class OpenGLController:
+	"""
+	PyQt5のQOpenGLWidgetを利用した三次元グラフィックス（OpenGL）のコントローラ（Controller of MVC）です。
+	"""
+
+	def __init__(self, view):
+		"""
+		OpenGLControllerのインスタンスを生成します。
+		"""
+		trace(self)
+
+		self._model = view._model
+		self._view = view
+
+	def close(self):
+		"""
+		ウィンドウを閉じて終了します。
+		"""
+		trace(self)
+
+		sys.exit(0)
+
+	def keyboard(self, event):
+		"""
+		キーボードが押されたときの処理をします。
+		"""
+		trace(self)
+
+		(lambda x: x) (event)
+
+	def motion(self, event):
+		"""
+		マウスの左ボタンを押しながらの移動を処理します。
+		"""
+		trace(self)
+
+		(lambda x: x) (event)
+
+	def mouse(self, event):
+		"""
+		マウスの左ボタンが押されたときの処理をします。
+		"""
+		trace(self)
+
+		(lambda x: x) (event)
---
+++ 043_Dragon/jp/ac/kyoto_su/cse/ap/python/OpenGLMVC/MVC/OpenGLView.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+例題プログラム：この例題を改変して大きなプログラムを作る足がかりにしてください。
+"""
+
+__author__ = 'AOKI Atsushi'
+__version__ = '0.4.2'
+__date__ = '2019/06/30 (Created: 2016/11/11)'
+
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QOpenGLVersionProfile
+from PyQt5.QtWidgets import QOpenGLWidget
+
+from jp.ac.kyoto_su.cse.ap.python.OpenGLMVC.MVC.OpenGLController import OpenGLController
+from jp.ac.kyoto_su.cse.ap.python.Trace import trace
+
+class OpenGLView(QOpenGLWidget):
+	"""
+	PyQt5のQOpenGLWidgetを利用した三次元グラフィックス（OpenGL）のビュー（View of MVC）です。
+	"""
+
+	def __init__(self, model, width=400, height=400, parent=None):
+		"""
+		OpenGLViewのインスタンスを生成します。
+		"""
+		trace(self)
+
+		super().__init__(parent)
+		self._model = model
+		self._controller = OpenGLController(self)
+		self._width = width
+		self._height = height
+		self._gl = None
+
+	def initializeGL(self):
+		"""
+		OpenGLの初期化を行います。
+		"""
+		trace(self)
+
+		version = QOpenGLVersionProfile()
+		version.setVersion(2, 1)
+
+		self._gl = self.context().versionFunctions(version)
+		self._gl.initializeOpenGLFunctions()
+
+		gl = self._gl
+		gl.glEnable(gl.GL_COLOR_MATERIAL)
+		gl.glEnable(gl.GL_DEPTH_TEST)
+		gl.glEnable(gl.GL_CULL_FACE)
+		gl.glEnable(gl.GL_NORMALIZE)
+		gl.glShadeModel(gl.GL_SMOOTH)
+
+		gl.glClearColor(0.9, 0.9, 0.9, 1.0)    # Red, Green, Blue, Alpha
+
+	def paintGL(self):
+		"""
+		OpenGLViewの中を描きます。
+		"""
+		trace(self)
+
+		gl = self._gl
+
+		gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+
+		# 絶対座標系（X軸：赤、Y軸：緑、Z軸：青）を描きます。負方向を1とすると、正方向は黄金比(1.618)になります。
+		self.rendering_axes(gl)
+
+	def rendering_axes(self, gl):
+		"""
+		世界座標系（絶対座標系）を描画（レンダリング）します。
+		"""
+		trace(self)
+
+		axes_scale = self._model._axes_scale
+		map_function = (lambda value: value * axes_scale)
+		scaled_by_n = (lambda vertex: list(map(map_function, vertex)))
+		gl.glBegin(gl.GL_LINES)
+		gl.glColor4f(1.0, 0.0, 0.0, 1.0)
+		gl.glVertex3fv(scaled_by_n([-1.000, 0.0, 0.0]))
+		gl.glVertex3fv(scaled_by_n([+1.618, 0.0, 0.0]))
+		gl.glColor4f(0.0, 1.0, 0.0, 1.0)
+		gl.glVertex3fv(scaled_by_n([0.0, -1.000, 0.0]))
+		gl.glVertex3fv(scaled_by_n([0.0, +1.618, 0.0]))
+		gl.glColor4f(0.0, 0.0, 1.0, 1.0)
+		gl.glVertex3fv(scaled_by_n([0.0, 0.0, -1.000]))
+		gl.glVertex3fv(scaled_by_n([0.0, 0.0, +1.618]))
+		gl.glEnd()
+
+	def resizeGL(self, width, height):
+		"""
+		OpenGLViewの大きさが変更された際に、OpenGLのビューポートを再形成します。
+		"""
+		trace(self)
+
+		self._width = width
+		self._height = height
+		extent = min(width, height)
+		x = (width - extent) // 2
+		y = (height - extent) // 2
+		w = extent
+		h = extent
+		self._gl.glViewport(x, y, w, h)
+
+	def sizeHint(self):
+		"""
+		OpenGLViewの大きさを応答します。
+		"""
+		trace(self)
+
+		return QSize(self._width, self._height)
+
+	def mouseMoveEvent(self, event):
+		"""
+		マウスが移動した際にコントローラに通知します。
+		"""
+		self._controller.motion(event)
+
+	def mousePressEvent(self, event):
+		"""
+		マウスボタンが押された際にコントローラに通知します。
+		"""
+		self._controller.mouse(event)
