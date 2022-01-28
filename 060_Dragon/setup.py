667857809893a5deb13f39ad5d8e4c0ac2b3b4f2
Kobayashi <shun123k@gmail.com>
2022-01-28 11:30:16 +0900

    モデル座標系を回転させる準備

--- 060_Dragon/setup.py
+++ 060_Dragon/setup.py
@@ -7,8 +7,8 @@ $ python setup.py sdist
 """
 
 __author__ = 'AOKI Atsushi'
-__version__ = '0.5.5'
-__date__ = '2019/07/01 (Created: 2016/11/11)'
+__version__ = '0.6.0'
+__date__ = '2019/07/02 (Created: 2016/11/11)'
 
 from distutils.core import setup
 import os, platform, re
